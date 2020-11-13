89b96f72265b5e04b7fe5f64ab4e347c229b1dc8
hiro <hiromhiro617@gmail.com>
2020-11-13 23:51:21 +0900

    ~p.108

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/books/:id" => "books#show"
+  delete "/books/:id" => "books#destroy"
+  resources :publishers
+  resource :profile, only: %i{show edit update}
+  # resources :publisher do
+  #   resources :books
+  #   member do
+  #     get 'detail'
+  #   end
+  #   collection do
+  #     get 'search'
+  #   end
+  # end
+
+
 end
